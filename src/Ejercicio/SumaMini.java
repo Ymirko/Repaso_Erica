fcb6f481a6e9c0370046498bfe977d262d22bf30
Ymirko <Ymirko@DESKTOP-V3P4O0Q>
2025-05-07 12:11:00 +0200

    subido070525

---
+++ src/Ejercicio/SumaMini.java
@@ -0,0 +1,10 @@
+package Ejercicio;
+import java.util.Scanner;
+
+public class SumaMini {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextDouble() + s.nextDouble());
+        s.close();
+    }
+}
